21cafea610ff42c62b1914f2c36e6e797ad0fe28
Alejandro Vozella <alejandrovozella@gmail.com>
2022-02-14 11:14:59 -0300

    Primer Commit del Proyecto

---
+++ src/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './style.css';
+const Footer = ()=>{
+    return(
+        <div className="footer">
+            <h3>AV Tecnology</h3>
+            <p>Copyright &copy; 2022</p>
+        </div>
+    )
+}
+
+export default Footer
