d5ba14597bbaf8c2fd059d5ee0d098461bf630ee
Alejandro Vozella <alejandrovozella@gmail.com>
2022-02-16 08:20:30 -0300

    Add Material-UI , Style Modyfi

--- src/Shared/Proyecto/Proyecto.js
+++ src/Shared/Proyecto/Proyecto.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './style.css';
+
 //import MaterialTable from 'material-table';
-import "bootstrap/dist/css/bootstrap.min.css"
+//import "bootstrap/dist/css/bootstrap.min.css"
 
 const Proyecto = () => {
     
     return ( 
         <div>
-            <table className="table table-dark">
+            {/* <table className="table table-dark">
               <thead >
                 <tr>
                     <th><h3>Participantes</h3></th>
@@ -16,8 +17,7 @@ const Proyecto = () => {
               <tbody>
                 <tr className="alumnos"><h4>Alejandro Vozella</h4></tr>
               </tbody>
-            </table>
-           
+            </table>            */}
         </div>
      );
 }
--- src/components/Inicio/Inicio.js
+++ src/components/Inicio/Inicio.js
@@ -6,7 +6,7 @@ import Proyecto from '../../Shared/Proyecto/Proyecto';
 const Inicio = ()=>{
     return(
         <div>
-            <h2>Segundo Parcial -MCGA</h2>
+            
             <Proyecto />
         </div>
     )
